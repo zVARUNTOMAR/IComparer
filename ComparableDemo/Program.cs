ff5dced12515af3af4a05d5860922ff74bcf81cb
Varun Tomar <varuntomar3301@outlook.com>
2021-03-16 22:30:18 +0530

    intial commit

---
+++ ComparableDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ComparableDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            User[] users = new User[4] { new User("Varun", 1998), new User("Ajay", 1977), new User("Atul", 1987),new User("Bob",2020) };
+
+            foreach (User i in users) {
+                Console.WriteLine("{0},{1}",i.name,i.dateOfBirth);
+            }
+
+            Console.WriteLine("========AFTER SORTING======");
+
+            Array.Sort(users);
+
+            foreach (User i in users) {
+                Console.WriteLine("{0},{1}",i.name,i.dateOfBirth);
+            }
+        }
+    }
+}
---
+++ ComparableDemo/User.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+namespace ComparableDemo
+{
+    public class User : IComparable
+    {
+
+        private class sortDateOfBirthHelper : IComparer {
+
+            int IComparer.Compare(object x, object y) {
+                User u1 = (User)x;
+                User u2 = (User)y;
+
+                if (u1.dateOfBirth > u2.dateOfBirth) {
+                    return 1;
+                }
+
+                if (u1.dateOfBirth < u2.dateOfBirth)
+                {
+                    return -1;
+                }
+
+                else {
+                    return 0;
+                }
+            }
+        }
+
+        int IComparable.CompareTo(object obj) {
+            User user1 = (User)obj;
+            return String.Compare(this.name, user1.name);
+        }
+
+        public string name { get; set; }
+
+        public int dateOfBirth { get; set; }
+
+        public User(string name, int dateOfBirth) {
+            this.name = name;
+            this.dateOfBirth = dateOfBirth;
+        }
+
+        public static IComparer sortDateOfBirth() {
+
+            return (IComparer)new sortDateOfBirthHelper();
+        }
+
+    }
+}
